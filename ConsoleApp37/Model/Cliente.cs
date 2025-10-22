9c01790778d61b0974b2940ee4db1f4deb13aeb6
elpibedelafacu <casanovasenzo2@gmail.com>
2025-10-22 20:25:48 -0300

    implemento-de-mvc

---
+++ ConsoleApp37/Model/Cliente.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cliente
+    {
+        public string Nombre { get; set; }
+        public double ID { get; set; }
+        public string Gmail { get; set }
+
+        public Cliente(string nombre,double id,string gmail)
+        {
+            Nombre = nombre;
+            ID = id;
+            Gmail = gmail;
+        }
+    }
+}
---
+++ ConsoleApp37/View/ClientView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace View
+{
+    public class ClientView
+    {
+        public void MostrarCliente(Cliente p)
+        {
+            Console.WriteLine($"Nombre{p.Nombre},Id{p.Id},Gmail{p.Gmail}")
+        }
+        public void ListaDeClientes(List<Cliente> clientes)
+        {
+            Console.WriteLine("lista de clientes");
+            foreach (var p in clientes)
+            {
+                Console.WriteLine($"Nombre{p.Nombre},Id{p.Id},Gmail{p.Gmail}");
+            }
+        } 
+        public void ValidarCorreo(string Gmail)
+        {
+
+        }
+    }
+}
